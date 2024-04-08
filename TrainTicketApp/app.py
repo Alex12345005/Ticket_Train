2ab1ea6f641e5f8b722d554242d39d5b590fbc33
Alex12345005 <rath190134@sr.htlweiz.at>
2024-04-08 14:58:40 +0200

    preisberechnung geupdated

--- TrainTicketApp/app.py
+++ TrainTicketApp/app.py
@@ -22,6 +22,36 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
+station_coordinates = {
+    "Graz Hauptbahnhof": (47.0667, 15.4333),
+    "Bruck an der Mur": (47.4105, 15.2772),
+    "Leoben Hauptbahnhof": (47.3820, 15.0946),
+    "Kapfenberg": (47.4425, 15.2860),
+    "Frohnleiten": (47.2739, 15.3194),
+    "Knittelfeld": (47.2153, 14.8227),
+    "Mürzzuschlag": (47.6039, 15.6728),
+    "Leibnitz": (46.7812, 15.5452),
+    "Fehring": (46.9311, 15.8950),
+    "Spielfeld-Straß": (46.7184, 15.6484),
+    "Weiz": (47.2167, 15.6167)
+}
+
+def calculate_distance(lat1, lon1, lat2, lon2):
+    from math import radians, cos, sin, sqrt, atan2
+
+    R = 6371.0  
+
+    lat1, lon1, lat2, lon2 = map(radians, [lat1, lon1, lat2, lon2])
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1-a))
+
+    distance = R * c
+    return distance
+
 def send_email_with_attachment(to_emails, subject, content, attachment_path):
     message = Mail(
         from_email='wadu0185@gmail.com',
@@ -51,31 +81,53 @@ def send_email_with_attachment(to_emails, subject, content, attachment_path):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    preis = None  
+    abfahrtsort = None  
+    zielort = None  
+
     if request.method == 'POST':
         name = request.form.get('name')
         zugnummer = request.form.get('zugnummer')
         abfahrtsort = request.form.get('abfahrtsort')
         zielort = request.form.get('zielort')
         abfahrtszeit = datetime.strptime(request.form.get('abfahrtszeit'), '%Y-%m-%dT%H:%M')
-        preis = float(request.form.get('preis'))
+        abfahrtsort_coords = station_coordinates.get(abfahrtsort)
+        zielort_coords = station_coordinates.get(zielort)
+
+        if abfahrtsort_coords and zielort_coords:
+            distanz = calculate_distance(*abfahrtsort_coords, *zielort_coords)
+            preis = distanz * 0.30 
+            preis = round(preis, 2)
 
         neue_buchung = Buchung(name=name, zugnummer=zugnummer, abfahrtsort=abfahrtsort, zielort=zielort, abfahrtszeit=abfahrtszeit, preis=preis)
-
         db.session.add(neue_buchung)
+
         try:
             db.session.commit()
-            return redirect(url_for('buchung_bestätigt', buchungs_id=neue_buchung.id))
         except IntegrityError:
             db.session.rollback()
-            flash('Ein Fehler ist aufgetreten. Die Buchung konnte nicht gespeichert werden.', 'error')
+            return "Ein Fehler ist aufgetreten. Die Buchung konnte nicht gespeichert werden.", 400
 
-    buchungen = Buchung.query.filter_by(storniert=False).all()
+        return redirect(url_for('buchung_bestätigt', buchungs_id=neue_buchung.id))
 
-    abfahrtsort = request.args.get('abfahrtsort', '')
-    zielort = request.args.get('zielort', '')
+    else:
+        abfahrtsort = request.args.get('abfahrtsort')
+        zielort = request.args.get('zielort')
+        preis = None  
 
-    return render_template('index.html', buchungen=buchungen, abfahrtsort=abfahrtsort, zielort=zielort)
-    
+        abfahrtsort_coords = None
+        zielort_coords = None
+
+        if abfahrtsort and zielort:
+            abfahrtsort_coords = station_coordinates.get(abfahrtsort)
+            zielort_coords = station_coordinates.get(zielort)
+
+            if abfahrtsort_coords and zielort_coords:
+                distanz = calculate_distance(*abfahrtsort_coords, *zielort_coords)
+                preis = distanz * 0.30  
+
+        buchungen = Buchung.query.all()
+        return render_template('index.html', buchungen=buchungen, preis=preis, abfahrtsort=abfahrtsort, zielort=zielort)    
 @app.route('/stornieren/<int:buchungs_id>')
 def stornieren(buchungs_id):
     buchung = Buchung.query.get_or_404(buchungs_id)
